04d717bfd0ed047ff5168f93d8c37ad03106a4f3
Huy Khánh <khanh.huynh0501008@hcmut.edu.vn>
2026-09-11 13:32:08 +0700

    Da xong bai 4

--- Bai_tap/IDE/STM32_project/Core/Inc/main.h
+++ Bai_tap/IDE/STM32_project/Core/Inc/main.h
@@ -64,6 +64,26 @@ void Error_Handler(void);
 #define LED_YELLOW_GPIO_Port GPIOA
 #define LED_GREEN_Pin GPIO_PIN_7
 #define LED_GREEN_GPIO_Port GPIOA
+#define a_Pin GPIO_PIN_0
+#define a_GPIO_Port GPIOB
+#define b_Pin GPIO_PIN_1
+#define b_GPIO_Port GPIOB
+#define c_Pin GPIO_PIN_2
+#define c_GPIO_Port GPIOB
+#define OTHER_LED_RED_Pin GPIO_PIN_8
+#define OTHER_LED_RED_GPIO_Port GPIOA
+#define OTHER_LED_YELLOW_Pin GPIO_PIN_9
+#define OTHER_LED_YELLOW_GPIO_Port GPIOA
+#define OTHER_LED_GREEN_Pin GPIO_PIN_10
+#define OTHER_LED_GREEN_GPIO_Port GPIOA
+#define d_Pin GPIO_PIN_3
+#define d_GPIO_Port GPIOB
+#define e_Pin GPIO_PIN_4
+#define e_GPIO_Port GPIOB
+#define f_Pin GPIO_PIN_5
+#define f_GPIO_Port GPIOB
+#define g_Pin GPIO_PIN_6
+#define g_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
