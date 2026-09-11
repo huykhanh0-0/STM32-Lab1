f4cef9f680576ffb7e58174992a3b4cadd40f5ed
Huy Khánh <khanh.huynh0501008@hcmut.edu.vn>
2026-09-11 11:20:18 +0700

    Cap nhat code Bai 2 tu ban sua loi (thu muc Download)

--- Bai_tap/IDE/STM32_project/Core/Inc/main.h
+++ Bai_tap/IDE/STM32_project/Core/Inc/main.h
@@ -64,12 +64,6 @@ void Error_Handler(void);
 #define LED_YELLOW_GPIO_Port GPIOA
 #define LED_GREEN_Pin GPIO_PIN_7
 #define LED_GREEN_GPIO_Port GPIOA
-#define OTHER_LED_RED_Pin GPIO_PIN_8
-#define OTHER_LED_RED_GPIO_Port GPIOA
-#define OTHER_LED_YELLOW_Pin GPIO_PIN_9
-#define OTHER_LED_YELLOW_GPIO_Port GPIOA
-#define OTHER_LED_GREEN_Pin GPIO_PIN_10
-#define OTHER_LED_GREEN_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
