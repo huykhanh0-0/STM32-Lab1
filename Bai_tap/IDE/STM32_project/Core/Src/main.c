302b62f6767227bce0f244df0b720238e936c773
Huy Khánh <khanh.huynh0501008@hcmut.edu.vn>
2026-09-11 10:40:35 +0700

    Hoan thanh Bai 3

--- Bai_tap/IDE/STM32_project/Core/Src/main.c
+++ Bai_tap/IDE/STM32_project/Core/Src/main.c
@@ -97,27 +97,45 @@ int main(void)
   int red_state = 1;
   int yellow_state = 0;
   int green_state = 0;
-  int count = 0;
+  int other_red_state = 0;
+  int other_yellow_state = 0;
+  int other_green_state = 1;
+  int count = 1;
+
   while (1)
   {
-  	  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, 1-red_state);
-      HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, 1-yellow_state);
-      HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, 1-green_state);
-      HAL_Delay((red_dur * red_state + yellow_dur * yellow_state + green_dur*green_state)*1000);
-      if(count == 0){
-    	  red_state = 1 - red_state;
-    	  yellow_state = 1 - yellow_state;
-      }
-      else if(count == 1){
-    	  yellow_state = 1 - yellow_state;
-    	  green_state = 1 - green_state;
-      }
-      else{
-    	  green_state = 1 - green_state;
-    	  red_state = 1 - red_state;
-    	  count = -1;
-      }
- 	  count++;
+
+	   HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, red_state);
+	   HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, yellow_state);
+	   HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, green_state);
+
+	   HAL_GPIO_WritePin(OTHER_LED_RED_GPIO_Port, OTHER_LED_RED_Pin,other_red_state);
+	   HAL_GPIO_WritePin(OTHER_LED_YELLOW_GPIO_Port, OTHER_LED_YELLOW_Pin, other_yellow_state);
+	   HAL_GPIO_WritePin(OTHER_LED_GREEN_GPIO_Port, OTHER_LED_GREEN_Pin, other_green_state);
+	   HAL_Delay(1000);
+
+	   if(count == green_dur){
+	 	  other_yellow_state = 1 - other_yellow_state;
+	 	  other_green_state = 1 - other_green_state;
+	   }
+	   else if(count == yellow_dur + green_dur){
+	 	  red_state = 1 - red_state;
+	 	  green_state = 1 - green_state;
+	 	  other_yellow_state = 1 - other_yellow_state;
+	 	  other_red_state = 1 - other_red_state;
+	   }
+	   else if (count == red_dur + green_dur){
+		  yellow_state = 1 - yellow_state;
+		  green_state = 1 - green_state;
+	   }
+	   else if(count == red_dur + yellow_dur + green_dur){
+	 	  red_state = 1 - red_state;
+	 	  yellow_state = 1 - yellow_state;
+	 	  other_red_state = 1 - other_red_state;
+	 	  other_green_state = 1 - other_green_state;
+	 	  count = 0;
+	   }
+	   count++;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -173,10 +191,13 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin|OTHER_LED_RED_Pin
+                          |OTHER_LED_YELLOW_Pin|OTHER_LED_GREEN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_GREEN_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin;
+  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_GREEN_Pin OTHER_LED_RED_Pin
+                           OTHER_LED_YELLOW_Pin OTHER_LED_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin|OTHER_LED_RED_Pin
+                          |OTHER_LED_YELLOW_Pin|OTHER_LED_GREEN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
